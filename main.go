e8f2bc75f25837dcf43eb12482ba087818750da8
Eva <dakoolstwunn@gmail.com>
2021-07-15 03:19:17 -0400

    Add main.go

--- main.go
+++ main.go
@@ -1,28 +1,19 @@
 package main
 
 import (
-	"github.com/gastrodon/groudon"
-	"git.gastrodon.io/imonke/monkebase"
-	"git.gastrodon.io/imonke/monkelib/middleware"
+	"github.com/brane-app/database-library"
+	"github.com/gastrodon/groudon/v2"
 
 	"log"
 	"net/http"
 	"os"
 )
 
-var (
-	bad_auth     = map[string]interface{}{"error": "bad_auth"}
-	expired_auth = map[string]interface{}{"error": "expired_auth"}
-)
-
 func main() {
-	monkebase.Connect(os.Getenv("MONKEBASE_CONNECTION"))
-	groudon.RegisterCatch(401, bad_auth)
-	groudon.RegisterMiddleware(middleware.MustAuth)
-	groudon.RegisterMiddleware(middleware.RejectBanned)
-	groudon.RegisterMiddleware(middleware.ParseMultipart)
-	groudon.RegisterMiddleware(transformBase64)
-	groudon.RegisterHandler("POST", "^/$", postContent)
-	http.Handle("/", http.HandlerFunc(groudon.Route))
+	database.Connect(os.Getenv("DATABASE_CONNECTION"))
+
+	register_handlers()
+
+	http.Handle(os.Getenv("PATH_PREFIX")+"/", http.HandlerFunc(groudon.Route))
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
