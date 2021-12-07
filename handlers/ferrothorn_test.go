ea28583211fa4ff583373ba3af0bb67c7375a15e
Eva Harris <mail@gastrodon.io>
2021-12-06 19:44:48 -0500

    Do dynamic masking
    Instead of constructing a file_url with a mask and storing it, only
    store the ferrothorn file_id, and construct it with a mask or host at
    runtime

--- handlers/ferrothorn_test.go
+++ handlers/ferrothorn_test.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"io"
-	"strings"
 	"testing"
 )
 
@@ -14,51 +13,7 @@ func (NopReader) Read(_ []byte) (_ int, err error) {
 }
 
 func Test_upload_prefix(test *testing.T) {
-	backupHost := ferrothorn_host
-	backupMask := ferrothorn_mask
-
-	defer func() {
-		ferrothorn_host = backupHost
-		ferrothorn_mask = backupMask
-	}()
-
-	ferrothorn_host = "host"
-	ferrothorn_mask = ""
-
-	url, err := upload(NopReader{})
-
-	if err != nil {
+	if _, err := upload(NopReader{}); err != nil {
 		test.Fatal(err)
-	}
-
-	if !strings.HasPrefix(url, ferrothorn_host) {
-		test.Fatalf("url doesn't have %s prefix: %s", ferrothorn_mask, url)
-	}
-}
-
-func Test_upload_prefixMasked(test *testing.T) {
-	backupHost := ferrothorn_host
-	backupMask := ferrothorn_mask
-
-	defer func() {
-		ferrothorn_host = backupHost
-		ferrothorn_mask = backupMask
-	}()
-
-	ferrothorn_host = "host"
-	ferrothorn_mask = "mask"
-
-	url, err := upload(NopReader{})
-
-	if err != nil {
-		test.Fatal(err)
-	}
-
-	if strings.HasPrefix(url, ferrothorn_host) {
-		test.Fatalf("url still has %s prefix: %s", ferrothorn_host, url)
-	}
-
-	if !strings.HasPrefix(url, ferrothorn_mask) {
-		test.Fatalf("url doesn't have %s prefix: %s", ferrothorn_mask, url)
 	}
 }
